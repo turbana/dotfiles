c4de96682a02aede145fe94cfa38c5ed14053fa4
Ian Clark <iclark@ewu.edu>
2015-04-17 16:16:34 -0700

    Xmonad test updates

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -13,7 +13,7 @@ import XMonad.Util.CustomKeys
 import XMonad.Hooks.DynamicLog
 import System.IO
 
-import XMonad.Util.Scratchpad
+import XMonad.Util.NamedScratchpad
 
 import XMonad.Layout.StackTile
 import XMonad.Layout.Grid
@@ -38,11 +38,11 @@ main = do
     normalBorderColor  = "#202020",
     focusedBorderColor = "#009900",
     layoutHook         = layoutHooks,
-    manageHook         = manageHooks,
+--    manageHook         = manageHooks,
+    manageHook         = manageHooks <+> manageDocks <+> namedScratchpadManageHook myScratchPads,
+--    manageHook         = namedScratchpadManageHook myScratchPads,
     logHook            = logHooks dzenLeftBar,
     keys               = customKeys emptyKeys myKeys
---    , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
---    , startupHook = setWMName "LG3D"
   }
 
 emptyKeys XConfig {modMask = modm} = []
@@ -58,14 +58,16 @@ myKeys conf@(XConfig {modMask = modm}) =
   , ((mod1Mask,               xK_h    ), sendMessage $ Go L)
   , ((mod1Mask,               xK_j    ), sendMessage $ Go U)
   , ((mod1Mask,               xK_k    ), sendMessage $ Go D)
-  , ((mod1Mask,               xK_comma), sendMessage (IncMasterN (-1)))
-  , ((mod1Mask,              xK_period), sendMessage (IncMasterN 1))
-  , ((0       ,               xK_Pause), spawn "calc")
+--  , ((mod1Mask,               xK_comma), sendMessage (IncMasterN (-1)))
+--  , ((mod1Mask,              xK_period), sendMessage (IncMasterN 1))
+--  , ((0       ,               xK_Pause), spawn "calc")
+  , ((0       ,               xK_Pause), namedScratchpadAction myScratchPads "calc")
+  , ((0       ,               xK_F1   ), namedScratchpadAction myScratchPads "orgCap")
   , ((mod1Mask,               xK_p    ), spawn "dmenu_run -m 0 -b -nb '#555555' -nf '#eeeeee' -sb '#003399' -sf '#ffffff' -p '>'")
   , ((mod1Mask .|. controlMask, xK_e  ), spawn "emacsclient -c -a ''")
-  , ((mod1Mask,               xK_F2   ), spawn "/home/iclark/src/ewu/whd/update-timetracking.sh")
-  , ((mod1Mask .|. controlMask, xK_q  ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
-  , ((mod1Mask,               xK_a    ), scratchPad)
+--  , ((mod1Mask,               xK_F2   ), spawn "/home/iclark/src/ewu/whd/update-timetracking.sh")
+  , ((mod1Mask .|. controlMask, xK_q  ), spawn "if type xmonad; then killall dzen2; xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
+--  , ((mod1Mask,               xK_a    ), scratchPad)
   ]
   ++
   -- mod-{q,w,e} to switch screens
@@ -74,15 +76,14 @@ myKeys conf@(XConfig {modMask = modm}) =
     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
-scratchPad = scratchpadSpawnActionTerminal "gnome-terminal"
+myScratchPads = [NS "calc" "gnome-terminal -t calculator -e calc"
+                 (title =? "calculator")
+                 (customFloating $ W.RationalRect (1/4) (1/4) (1/2) (1/2)),
 
-manageScratchPad :: ManageHook
-manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
-  where
-    h = 0.1     -- terminal height, 10%
-    w = 1       -- terminal width, 100%
-    t = 1 - h   -- distance from top edge, 90%
-    l = 1 - w   -- distance from left edge, 0%
+                 NS "orgCap" "emacsclient -c -F '(quote (name . \"OrgCapture\"))' -e '(org-capture)'"
+                 (className =? "OrgCapture")
+                 (customFloating $ W.RationalRect (1/2) (1/2) (1/2) (1/2))
+                ]
 
 
 layoutHooks =
@@ -90,27 +91,37 @@ layoutHooks =
   desktopLayoutModifiers $
   onHost hostHome (tall ||| Full) $              -- Home layouts (below this are work layouts)
   onWorkspace "2" (Mirror tallLarge ||| Full) $
---  reflectVert $ Mirror tall ||| Full
-  reflectVert tile ||| reflectVert tileMany ||| windowNavigation combined ||| Full
+  reflectVert tile ||| Full
+--  reflectVert tile ||| reflectVert tileMany ||| windowNavigation combined ||| Full
   where
     tall       = Tall      nmaster delta golden
     tallLarge  = Tall      nmaster delta 0.70
     tile       = StackTile nmaster delta 0.57
     tileMany   = StackTile nmaster delta 0.25
-    combined   = combineTwo (Mirror $ TwoPane 0.03 0.75) (reflectVert Grid) (tile)
+    --combined   = combineTwo (Mirror $ TwoPane 0.03 0.75) (reflectVert Grid) (tile)
     nmaster    = 1
     delta      = 0.03
     golden     = toRational (2/(1 + sqrt 5 :: Double))
     half       = 0.5
     third      = 0.33
 
-      
-manageHooks = composeAll [
-    className =? "Icedove" --> doShift "2",
+
+manageHooks = composeOne [
+    className =? "Pidgin"  -?> doShift "2",
+    className =? "Pidgin"  -?> doF avoidMaster
+  ]
+  where
+    avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
+    avoidMaster = W.modify' $ \c -> case c of
+      W.Stack t [] (r:rs) ->  W.Stack t [r] rs
+      otherwise           -> c
+
+
+old_manageHooks = composeAll [
+    namedScratchpadManageHook myScratchPads,
     className =? "Pidgin"  --> doShift "2",
-    className =? "Pidgin"  --> doF avoidMaster,
-    manageDocks,
-    manageScratchPad
+    className =? "Pidgin"  --> doF avoidMaster--,
+--    manageDocks
   ]
   where
     avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
