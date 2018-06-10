e8a268a98e1e2c312b58b394a98d33db597b494c
Ian Clark <turbana@gmail.com>
2018-06-10 06:26:24 -0700

    Cleaned up dzen scripts

--- xmonad/dzen2-left-bar.sh
+++ xmonad/dzen2-left-bar.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#font="-*-fixed-medium-r-*-*-12-*-*-*-*-*-iso8859-*"
-#font="Monospace-8"
 font="DejaVu Sans Mono-9"
-fg="#d9d9d9"
-bg="#262626"
+fg=$(getcolor base+3)
+bg=$(getcolor base-3)
+bar=$(getcolor base-2)
 
-# I don't see a way to configure what Xmonad is sending us without reimplementing
-# an entire logger. I don't want to see what layout mode we're in so strip everything
-# between the two | with sed
+# I don't see a way to configure what Xmonad is sending us without
+# re-implementing an entire logger. I don't want to see what layout mode we're
+# in so strip everything between the two | with sed
 sed -ue 's/|[^|]*|/|/' | \
 # start with a space
 sed -ue 's/^/ /' | \
 # insert our bar highlight
-sed -ue 's/$/^pa(0)^fg(#595959)^ib(1)^ro(1420x1-0-9)^ib(0)/' | \
-dzen2 -y 1060 -x 0 -w 1420 -ta l -h 20 -fg $fg -bg $bg -fn "$font" -xs 1 -e 'onstart=lower'
+sed -ue 's/$/^pa(0)^fg('"$bar"')^ib(1)^ro(1420x1-0-9)^ib(0)/' | \
+dzen2 -y 1060 -x 0 -w 1420 -ta l -h 20 -fg $fg -bg $bg -fn "$font" -xs 1 \
+	-e 'onstart=lower'
--- xmonad/dzen2-right-bar.sh
+++ xmonad/dzen2-right-bar.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-#font="-*-fixed-medium-r-*-*-12-*-*-*-*-*-iso8859-*"
-#font="Monospace-8"
 font="DejaVu Sans Mono-9"
-fg="#d9d9d9"
-bg="#262626"
+fg=$(getcolor base+3)
+bg=$(getcolor base-3)
 
 python ~/.xmonad/cpugraph.py | \
-/home/ian/downloads/dzen/dzen2 -y 1060 -x 1420 -w 500 -ta r -sa r -h 20 -fg $fg -bg $bg -fn "$font" -l 16 -e 'button1=togglecollapse'
-#dzen2 -y 1060 -x 1420 -w 500 -ta r -sa r -h 20 -fg $fg -bg $bg -fn "$font" -l 16 -e 'button1=togglecollapse'
-#dzen2 -y -1 -x -500 -w 500 -ta r -sa r -h 20 -fg $fg -bg $bg -fn $font -xs 1 -l 16 -e 'button1=togglecollapse'
-#dzen2 -y -1 -x -6000 -w 6000 -ta r -h 20 -fg $fg -bg $bg -fn $font -e 'onstart=lower' -xs 1 -l 2
+dzen2 -y 1060 -x 1420 -w 500 -ta r -sa r -h 20 -fg $fg -bg $bg -fn "$font" \
+	-l 16 -e 'button1=togglecollapse'
