b836b761d3fe6d8d3d668fcbc5cec7179131a5d3
Ian Clark <turbana@gmail.com>
2019-02-13 11:58:45 -0700

    Updated xmonad key config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -23,52 +23,57 @@ import XMonad.Layout.WindowNavigation --XXX
 import XMonad.ManageHook
 import XMonad.Util.Cursor
 import XMonad.Util.CustomKeys
+import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run
 
 hostHome = "cyclone"
 
+myWorkspaces = map show [1..9]
+
+dzenBackground = "#262626"
+
+myCommand "editor" = "emacsclient -c -a '' --eval '(spacemacs/home)'"
+myCommand "dmenu"  = "dmenu_run -b -nb '#333333' -nf '#eeeeee' -sb '#afaf00' -sf '#000000' -p '>'"
+myCommand "xmonad" = "if type xmonad; then killall dzen2; xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi"
+myCommand "left-status-bar" = "$HOME/.xmonad/dzen2-left-bar.sh"
+myCommand "right-status-bar" = "$HOME/.xmonad/dzen2-right-bar.sh"
+
 main = do
-  dzenLeftBar <- spawnPipe myXmonadBar
-  spawn myStatusBar
-  xmonad $ docks def {
+  dzenLeftBar <- spawnPipe $ myCommand "left-status-bar"
+  xmonad $ docks $ myConfig dzenLeftBar `additionalKeysP` myKeys_
+
+myConfig h = def {
     terminal           = "gnome-terminal",
-    startupHook        = setDefaultCursor xC_left_ptr,
+    startupHook        = spawn $ myCommand "right-status-bar",
     borderWidth        = 2,
     normalBorderColor  = "#000000",
     focusedBorderColor = "#67e671",
     manageHook         = manageHooks,
     layoutHook         = layoutHooks,
-    logHook            = logHooks dzenLeftBar,
-    keys               = customKeys emptyKeys myKeys
+    logHook            = logHooks h
   }
 
-emptyKeys XConfig {modMask = modm} = []
-
-myKeys conf@(XConfig {modMask = modm}) =
-  [ ((mod1Mask,               xK_k    ), windows W.focusUp  )
-  , ((mod1Mask,               xK_j    ), windows W.focusDown)
-  , ((mod1Mask .|. shiftMask, xK_k    ), windows W.swapUp)
-  , ((mod1Mask .|. shiftMask, xK_j    ), windows W.swapDown)
-  , ((mod1Mask .|. shiftMask, xK_l    ), sendMessage Shrink)
-  , ((mod1Mask .|. shiftMask, xK_h    ), sendMessage Expand)
-  , ((mod1Mask,               xK_l    ), sendMessage $ Go R)
-  , ((mod1Mask,               xK_h    ), sendMessage $ Go L)
-  , ((mod1Mask,               xK_k    ), sendMessage $ Go U)
-  , ((mod1Mask,               xK_j    ), sendMessage $ Go D)
-  , ((0       ,               xK_Pause), namedScratchpadAction myScratchPads "calc")
-  , ((0       ,               xK_F1   ), namedScratchpadAction myScratchPads "orgCap")
-  , ((mod1Mask,               xK_p    ), spawn "dmenu_run -b -nb '#333333' -nf '#eeeeee' -sb '#afaf00' -sf '#000000' -p '>'")
-  , ((mod1Mask .|. controlMask, xK_e  ), spawn "emacsclient -c -a '' --eval '(spacemacs/home)'")
-  , ((mod1Mask .|. controlMask, xK_q  ), spawn "if type xmonad; then killall dzen2; xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
-  , ((mod1Mask .|. controlMask, xK_s  ), sendMessage ToggleStruts)
+myKeys_ =
+  [("M-j", windows W.focusDown)
+  ,("M-k", windows W.focusUp)
+  ,("S-M-j", windows W.swapDown)
+  ,("S-M-k", windows W.swapUp)
+  ,("<Pause>", namedScratchpadAction myScratchPads "calc")
+  ,("<F1>", namedScratchpadAction myScratchPads "orgCap")
+  ,("M-p", spawn $ myCommand "dmenu")
+  ,("M-o", spawn $ myCommand "editor")
+  ,("C-M-x r", spawn $ myCommand "xmonad")
+  ,("c-M-x s", sendMessage ToggleStruts)
   ]
   ++
-  -- mod-{q,w,e} to switch screens
-  [((m .|. mod1Mask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-    | (key, sc) <- zip [xK_q, xK_w, xK_e] [2,0,1]
-    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+  -- bind M-# and S-M-# to navigate or move a window to workspace #
+  [ (otherModMasks ++ "M-" ++ key, action tag)
+  | (tag, key)  <- zip myWorkspaces myWorkspaces
+  , (otherModMasks, action) <- [ ("", windows . W.greedyView) -- or W.view
+                               , ("S-", windows . W.shift)]
+  ]
 
 
 myScratchPads = [
@@ -109,10 +114,6 @@ manageHooks = composeAll [
       W.Stack t [] (r:rs) ->  W.Stack t [r] rs
       otherwise           -> c
 
-
-myXmonadBar = "$HOME/.xmonad/dzen2-left-bar.sh"
-myStatusBar = "$HOME/.xmonad/dzen2-right-bar.sh"
-dzenBackground = "#262626"
 
 logHooks h = dynamicLogWithPP . namedScratchpadFilterOutWorkspacePP $ defaultPP {
   ppCurrent           = dzenColor "#ffffff" dzenBackground,
