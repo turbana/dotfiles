dcaae6998ff8167c70ab6b1cada1d983bde18efb
Ian Clark <turbana@gmail.com>
2026-02-27 10:27:28 -0800

    conky now waits for second monitor

--- conky/conky.sh
+++ conky/conky.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 
+# delay conky start until the second monitor turns on
+sleep 10
+# XXX why doesn't this work?
+# wait for second display to connect
+# while ! (xrandr --query | grep -qP '\bconnected (?!primary)'); do
+#     sleep 1
+# done
+
+/usr/bin/conky -c conky.config 
